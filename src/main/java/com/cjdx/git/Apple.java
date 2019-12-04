a92290d5e08d8f7b535079a60339e1eeb7187236
tom_ttl <eclipse_user@qq.com>
2019-12-04 16:38:27 +0800

    My first commit in eclipse

---
+++ src/main/java/com/cjdx/git/Apple.java
@@ -0,0 +1,7 @@
+package com.cjdx.git;
+//?»¹Ã»ÓÐadd
+public class Apple {
+	public static void main(String[] args) {
+		System.out.println("hahaha");
+	}
+}
---
+++ src/test/java/com/cjdx/Git/TestGit.java
@@ -0,0 +1,5 @@
+package com.cjdx.Git;
+
+public class TestGit {
+
+}
